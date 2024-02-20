2fe8383067df1cbdce7b8c835074f37f891247a8
song-eunhye <dmsgp8292@gmail.com>
2024-02-20 16:16:29 +0900

    first commit

---
+++ src/main/java/com/seed_planner/schedule_center/ScheduleCenterApplication.java
@@ -0,0 +1,13 @@
+package com.seed_planner.schedule_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seed_planner/schedule_center/ScheduleCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seed_planner.schedule_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
