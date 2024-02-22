2bf8d7d3287d0d36cb7f032371228b379342d48c
song-eunhye <dmsgp8292@gmail.com>
2024-02-22 17:39:57 +0900

    [ADD] - BaseDomain

---
+++ src/main/java/com/seed_planner/schedule_center/common/BaseDomain.java
@@ -0,0 +1,13 @@
+package com.seed_planner.schedule_center.common;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public abstract class BaseDomain {
+    private String id;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+    private boolean isDeleted;
+}
