2606c23f3a1f1e683632f00b76783bb1b805f61c
song-eunhye <dmsgp8292@gmail.com>
2024-03-15 17:26:28 +0900

    [UPDATE] - 프론트 로컬 CORS 허용

--- src/main/java/com/seed_planner/schedule_center/common/WebConfig.java
+++ src/main/java/com/seed_planner/schedule_center/common/WebConfig.java
@@ -3,6 +3,8 @@ package com.seed_planner.schedule_center.common;
 import com.seed_planner.schedule_center.common.intercepter.AuthInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -22,5 +24,17 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(authInterceptor)
             .addPathPatterns("/**")
             .excludePathPatterns(EXCLUDE_PATHS);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods(HttpMethod.GET.name(),  HttpMethod.PUT.name(), HttpMethod.POST.name(), HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name())
+            .exposedHeaders("Authorization")
+            .allowCredentials(true)
+            .maxAge(3600);
+
     }
 }
