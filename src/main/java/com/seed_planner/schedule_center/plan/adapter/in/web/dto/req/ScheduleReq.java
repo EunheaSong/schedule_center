e01a69b0bbfd0ad6a8a01a292c42e6884a7d8042
eunhyes <dmsgp8292@gmail.com>
2024-03-22 17:38:39 +0900

    [ADD] - scheduleServiceTest part 1.

---
+++ src/main/java/com/seed_planner/schedule_center/plan/adapter/in/web/dto/req/ScheduleReq.java
@@ -0,0 +1,30 @@
+package com.seed_planner.schedule_center.plan.adapter.in.web.dto.req;
+
+import com.seed_planner.schedule_center.plan.domain.Location;
+import com.seed_planner.schedule_center.plan.domain.Memo;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+public class ScheduleReq {
+    @NotBlank
+    @Length(max = 50)
+    private String title;
+    @NotEmpty(message = "")
+    private LocalDateTime startedAt;
+    @NotEmpty(message = "")
+    private LocalDateTime endedAt;
+    @Length(max = 50)
+    private String place;
+    private String imagePath;
+    private String[] participantsId;
+    private String categoryId;
+    private Location location;
+    private Memo memo;
+}
