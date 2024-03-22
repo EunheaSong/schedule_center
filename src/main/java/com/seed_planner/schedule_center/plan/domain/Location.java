0a3c5e2f8a8d3249b358954bc8f2f6d8c1bdfc4a
eunhyes <dmsgp8292@gmail.com>
2024-03-22 17:37:52 +0900

    [UPDATE] - update Location validation

--- src/main/java/com/seed_planner/schedule_center/plan/domain/Location.java
+++ src/main/java/com/seed_planner/schedule_center/plan/domain/Location.java
@@ -1,20 +1,31 @@
 package com.seed_planner.schedule_center.plan.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-/**
+import lombok.AllArgsConstructor;
+import lombok.Getter; /**
  * Open API를 통해 받아온 주소 데이터를 담는 모델
  */
-@AllArgsConstructor
 @Getter
 public class Location {
     private Double lat;
     private Double lng;
     private String address;
 
+    public Location(Double lat, Double lng, String address) {
+        valid(lat, lng);
+        this.lat = lat;
+        this.lng = lng;
+        this.address = address;
+    }
+
+    private void valid(Double lat, Double lng) {
+        if (!(lat != null && lng != null)) throw new IllegalArgumentException("");
+    }
+
     @Override
     public String toString() {
         return "Lat : " + this.lat + ", Lng : " + this.lng + ", Address : " + this.address;
     }
+
 }
+
