cc6c2e8652fdddf6412606c887cdf247f2cf49a7
song-eunhye <dmsgp8292@gmail.com>
2024-03-23 17:53:52 +0900

    [ADD] - controller test에 content 삽입을 위한 gson utils 생성

---
+++ src/test/java/com/seed_planner/schedule_center/common/GsonUtils.java
@@ -0,0 +1,31 @@
+package com.seed_planner.schedule_center.common;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class GsonUtils {
+
+    public static class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
+        private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+
+        @Override
+        public LocalDateTime read(JsonReader in) throws IOException {
+            return LocalDateTime.parse(in.nextString(), formatter);
+        }
+
+        @Override
+        public void write(JsonWriter jsonWriter, LocalDateTime localDateTime) throws IOException {
+            if (localDateTime == null) {
+                jsonWriter.nullValue();
+            } else {
+                jsonWriter.value(formatter.format(localDateTime));
+            }
+        }
+    }
+}
+
